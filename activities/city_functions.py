0d16f7562cc048aeba479316e55dfb66dc013d30
21garcbe <21garcbe@gmail.com>
2026-04-03 11:43:28 -0400

    added population to city_functions without making optional

--- activities/city_functions.py
+++ activities/city_functions.py
@@ -5,12 +5,22 @@ and population (integer) into a single string in the format "City, Country".
 
 """
 
-def format_city_country(city, country):
-    """Takes a city name and country name formats it as 'City, Country'."""
-    return f"{city}, {country}"
+def format_city_country(city, country, population):
+    """Takes a city name, country name and optional population formats it as 'City, Country'."""
+    if population is not None:
+        formatted_location = f"{city}, {country} - Population: {population}"
+    else:
+        formatted_location = f"{city}, {country}"
+    return formatted_location
 
+def main():
+    city = input("Enter the name of a city: ")
+    country = input("Enter the name of a country: ")
+    population = input("Enter the population (optional): ")
 
-city = input("Enter the name of a city: ")
-country = input("Enter the name of a country: ")
+    
 
-print(format_city_country(city, country))
+    print(format_city_country(city, country, population))
+
+if __name__ == "__main__":
+    main()
